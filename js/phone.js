3970aeefd75c99d06c65ca7427bbd1595c4a24e4
jrazad10 <jrazad10@gmail.com>
2024-03-01 18:50:32 +0600

    partial updated of this module

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,104 @@
-console.log("con");
+const loadPhone = async (searchText, isShowAll) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+    const data = await res.json();
+    const phones = data.data;
+    // console.log(phones);
+    displayPhones(phones, isShowAll);
+
+}
+
+
+const displayPhones = (phones, isShowAll) => {
+    console.log(phones);
+
+    const phoneContainer = document.getElementById('phone-container')
+    //clear phone container cards before adding new cards
+    phoneContainer.textContent = '';
+
+    //display show all button if there are more than 12
+    const showAllConatiner = document.getElementById('show-all-container')
+    if (phones.length > 12 && !isShowAll) {
+        showAllConatiner.classList.remove('hidden');
+    } else {
+        showAllConatiner.classList.add('hidden')
+    }
+    console.log("is show all", isShowAll);
+
+    // display only 1st 12 phones if now show all
+    if (!isShowAll) {
+        phones = phones.slice(0, 12);
+    }
+    phones.forEach(phone => {
+        console.log(phone);
+        //2. create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
+        //3. set inner html
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}"
+                            alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">${phone.phone_name}</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-center">
+                            <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
+                        </div>
+                    </div>
+        `;
+        //4. append child
+        phoneContainer.appendChild(phoneCard);
+
+        // hide loading spinner
+        toggleLoadingSpinner(false);
+
+    })
+}
+
+//
+const handleShowDetail = async (id) => {
+    console.log('clicked show details', id);
+    //load single phone data 
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    console.log(data);
+
+}
+
+
+//handle search button
+const handleSearch = (isShowAll) => {
+    toggleLoadingSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText, isShowAll);
+}
+
+//handle search recap
+// const handleSearch2 = () => {
+//     toggleLoadingSpinner(true);
+//     const searchField = document.getElementById('search-field2');
+//     const searchText = searchField.value;
+//     loadPhone(searchText);
+// }
+
+const toggleLoadingSpinner = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if (isLoading) {
+        loadingSpinner.classList.remove('hidden');
+    } else {
+        loadingSpinner.classList.add('hidden');
+    }
+}
+
+//handle show all
+
+const handleShowAll = () => {
+    handleSearch(true)
+}
+
+
+
+    // loadPhone();
+
+    ;
